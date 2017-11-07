972a3ef640964eb01da8ded92bb44c5ca3b05385
Akira Yumiyama <yumiyama.akira@gmail.com>
2017-11-07 09:41:01 +0900

    シート名にクエリIDを含めて、対応するシートを更新するように調整する

--- redash/tasks/google_spreadsheet.py
+++ redash/tasks/google_spreadsheet.py
@@ -12,6 +12,8 @@ from gspread.utils import rowcol_to_a1
 from oauth2client.client import OAuth2Credentials
 import json
 import datetime
+import os
+from pytz import timezone
 
 
 @celery.task(name="redash.tasks.export_google_spreadsheet")
@@ -51,29 +53,34 @@ def export_google_spreadsheet(query_id):
     worksheet_id = None
     if '#gid=' in spreadsheet_url:
         worksheet_id = spreadsheet_url.split('#', 2)[1].replace('gid=', '')
+        logger.info("query_id={} (worksheet_id={})".format(query_id, worksheet_id))
+
     worksheet = None
-    if worksheet_id:
-        for ws in spreadsheet.worksheets():
-            if ws.id == worksheet_id:
-                worksheet = ws
-                break
-    if worksheet is None:
-        for ws in spreadsheet.worksheets():
-            if ws.title.startswith("redash-export:"):
-                worksheet = ws
-                break
-    if worksheet is None:
-        worksheet = spreadsheet.get_worksheet(0)
-        worksheet.update_title("redash-export")
+    for ws in spreadsheet.worksheets():
+        if worksheet:
+            break
+        if ws.id == worksheet_id:
+            worksheet = ws
+    for ws in spreadsheet.worksheets():
+        if worksheet:
+            break
+        if ws.title.startswith("redash: query={}".format(query_id)):
+            worksheet = ws
+
+    if worksheet:
+        logger.info("query_id={} (worksheet detect: {})".format(query_id, worksheet))
+    else:
+        worksheet = spreadsheet.add_worksheet(title="redash: query={}".format(query_id), rows="100", cols="20")
+        logger.info("query_id={} (worksheet created. {})".format(query_id, worksheet))
 
     worksheet.resize(len(rows) + 1, len(columns))
     cell_list = worksheet.range(range_addr)
     for i, cell in enumerate(cell_list):
         cell.value = payload[i]
     worksheet.update_cells(cell_list)
-    worksheet.update_title("redash-export: execute={},update={}".format(
-        query_result.retrieved_at.strftime('%Y/%m/%d %H:%M'),
-        datetime.datetime.utcnow().strftime('%Y/%m/%d %H:%M')
+    worksheet.update_title("redash: query={} execute={}".format(
+        query_id,
+        query_result.retrieved_at.astimezone(timezone(os.environ.get('TZ', 'UTC'))).strftime('%Y/%m/%d %H:%M')
     ))
 
 
